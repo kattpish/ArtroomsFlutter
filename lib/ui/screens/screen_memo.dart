2c1e0ee7937c8064d46e8a6cbbc4f8d495e3feb8
Ituzelalande <133678816+Ituzelalande@users.noreply.github.com>
2024-02-06 14:47:15 +0200

    chatroom drawer

--- lib/ui/screens/screen_memo.dart
+++ lib/ui/screens/screen_memo.dart
@@ -17,7 +17,50 @@ class _MyScreenMemoState extends State<MyScreenMemo> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios,color: Colors.grey[400]),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('메모'),
+        elevation: 0,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Container(
+            color: Colors.grey[200],
+            height: 1.0,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: TextFormField(
+          maxLines: null,
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(
+            hintText: '메모를 입력하세요',
+              border: InputBorder.none
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(4.0),
+        decoration: BoxDecoration(color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: TextButton(
+          onPressed: (){
+          },
+          child:Text('저장',style: TextStyle(color: Colors.white,
+          fontSize: 20,)),
+
+          ),
+      ),
+    );
   }
 
 }
