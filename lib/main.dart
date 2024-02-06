00d2ac0fd7ff28653cd0764039e1f74649d67729
Ituzelalande <133678816+Ituzelalande@users.noreply.github.com>
2024-02-06 14:47:15 +0200

    chatroom drawer

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@
 import 'package:artrooms/ui/screens/screen_chatroom_drawer.dart';
 import 'package:artrooms/ui/screens/screen_chats.dart';
 import 'package:artrooms/ui/screens/screen_login.dart';
+import 'package:artrooms/ui/screens/screen_memo.dart';
 import 'package:flutter/material.dart';
 
 
 void main() {
   runApp(
     const MaterialApp(
-      home: MyScreenLogin(),
+      home: MyScreenMemo(),
       debugShowCheckedModeBanner: false,
     ),
   );
