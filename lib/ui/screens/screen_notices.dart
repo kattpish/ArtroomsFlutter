521e87c73276d53e7f469088b07d97528cb07c5d
Ituzelalande <133678816+Ituzelalande@users.noreply.github.com>
2024-02-07 22:11:08 +0200

    chatroom drawer

--- lib/ui/screens/screen_notices.dart
+++ lib/ui/screens/screen_notices.dart
@@ -1,6 +1,7 @@
 
 import 'dart:js_interop';
 
+import 'package:artrooms/ui/theme/theme_colors.dart';
 import 'package:flutter/material.dart';
 
 
@@ -27,7 +28,7 @@ class Notification {
 }
 class _MyScreenNoticesState extends State<MyScreenNotices> {
   final List<Notification> notifications = [
-    Notification("2024.5.12", "프로젝트 공지", "다음주는 쉽니다!", isVisible: true ),
+    Notification("2024.5.12", "프로젝트 공지", "* 첫 수업 공지사항  1.수업 전에 디스코드를 깔아 주세요 2. 마이크를 준비해주세요.  3. 이전에 그렸던 그림이 있다면", isVisible: true ),
     Notification("2024.5.5", "", "미팅이 오전으로 변경되었습니다.",  isVisible: false )
   ];
 
@@ -57,43 +58,74 @@ class _MyScreenNoticesState extends State<MyScreenNotices> {
     return Scaffold(
 
       appBar: AppBar(
-        title: Text('Notifications'),
+        leading: Icon(Icons.arrow_back_ios,color:colorMainGrey200,),
+        title: Text('공지'),
       ),
       body: ListView(
         children: notifications.map((notification) {
           return Card(
-            margin: EdgeInsets.all(8.0),
+            margin: EdgeInsets.all(10.0),
             elevation: 0,
             child: Column(
+
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
+
               children:  [
+                Container(
+                  height: 1.0,
+                  width: double.infinity,
+                  color: colorMainGrey200,
+                ),
                 Row(
                   children: [
                     Text(
                       notification.date,
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue
                       ),
                     ),
+
                     Visibility(
-                      visible: notification.isVisible,
-                      child: Icon(Icons.star),
+
+                        visible: notification.isVisible,
+                        child: Container(
+                          child:Icon(Icons.star,size:10, color: Colors.white,),
+                          padding: EdgeInsets.all(2.0),
+                          margin: EdgeInsets.all(6.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            shape:  BoxShape.circle,
+                          ),)
+
                     ),
+
                     Text(
                       notification.title,
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 15,
                       ),
                     )
                   ],
                 ),
-                  Container(
-                    child: Text(notification.body),
-                      padding: EdgeInsets.all(16.0),
+                Container(
+                  height: 1.0,
+                  width: double.infinity,
+                  color: colorMainGrey200,
+                ),
+                Container(
+                  child: Text(notification.body),
+                  padding: EdgeInsets.all(16.0),
 
-                  )
-                  //
+                ),
+                Container(
+                  height: 1.0,
+                  width: double.infinity,
+                  color: colorMainGrey200,
+                ),
+                //
               ],),);
         }).toList(),
       ),
